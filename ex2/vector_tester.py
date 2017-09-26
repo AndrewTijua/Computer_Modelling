4c2c69e4c4ce4799c76e93cb2a6d13be5c0bbe79
Benjamin Cox <s1621312@cplab042.ph.ed.ac.uk>
2017-09-26 16:26:44 +0100

    Vector tester
    Added more to vector tester

--- ex2/vector_tester.py
+++ ex2/vector_tester.py
@@ -8,3 +8,11 @@ vec_3 = [random(),random(),random()]
 print("v_1 = {0}, |v_1| = {1}".format(vec_1, vector.mag(vec_1)))
 print("v_2 = {0}, |v_2| = {1}".format(vec_2, vector.mag(vec_2)))
 print("v_3 = {0}, |v_3| = {1}".format(vec_3, vector.mag(vec_3)))
+
+print("\nv_1 + v_2 = {0}".format(vector.vec_sum(vec_1, vec_2)))
+
+print("\nv_1 . v_2 = {0}".format(vector.vec_dot(vec_1, vec_2)))
+
+print("\nv_1 x v_2 = {0}".format(vector.vec_cross(vec_1, vec_2)))
+
+print("\n\n---Testing Identities---\n\n")
